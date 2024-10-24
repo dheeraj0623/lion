28f9df4df255db5e9148adac2b1f860c77d66387
dheeraj0623 <gupta.dheeraj23@gmail.com>
2024-10-24 08:33:00 +0530

    first time commit

---
+++ demo.py
@@ -0,0 +1,3 @@
+import test
+print(test.add(2,4))
+print(test.sub(10,5))
---
+++ testing.py
@@ -0,0 +1,7 @@
+from random import *
+print(randint(0,9),
+      chr(randint(65,90)),
+      randint(0,9),
+      chr(randint(65,90)),
+      randint(0,9),
+      chr(randint(65,90)))
